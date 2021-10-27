645df22634bbc0834de1c254451828884b18aa85
Jonas Antunes <jonas.antunes@involves.com>
2021-10-27 07:49:28 -0300

    submit prevent default

---
+++ www/js/form.js
@@ -0,0 +1,6 @@
+const formContact = document.querySelector("#contact-us");
+
+formContact.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.warn('Submit contact Method =>')
+});
